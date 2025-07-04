f6b965e9fb43e5917d5cd5d1f87c510622aad6aa
DarkFoxGnS <szabotibor123456789@gmail.com>
2025-07-04 16:13:11 +0200

    - TextEncoder.py renamed to DataEncoder.py - Not required code removed from DataEncoder.py and   SteganographyCodec.py

---
+++ DataEncoder.py
@@ -0,0 +1,109 @@
+import hashlib
+from argon2 import low_level
+import hmac
+
+def hmac_salt (password: bytes, salt: bytes) -> bytes:
+    """
+    Creates an hmac encoded hash.
+    
+    Params:
+        password: The password as a byte array.
+        salt: The salt to be encoded.
+
+    Returns:
+        Returns a byte array of encoded data.
+    """
+    return hmac.new(password,salt, hashlib.sha256).digest()
+
+
+def argon2_hash(password: bytes, salt: bytes, hash_len=64) -> bytes:
+    """
+    Hashes using argon2.
+    
+    Params:
+        password: The password as a byte array.
+        salt: The salt as a byte array.
+        hash_len: The length of the return hash
+
+    Returns:
+        Byte array of the hash.
+    """
+    return low_level.hash_secret_raw(
+        secret = password,
+        salt = salt,
+        time_cost=3,
+        memory_cost = 65536,
+        parallelism = 2,
+        hash_len = hash_len,
+        type = low_level.Type.ID
+        )
+
+def make_password(password_bytes: bytes,length: int) -> bytes:
+    """
+    Generates the encryption password.
+        
+    Params:
+        password_bytes: Byte array of password.
+        length: The amound of passwords to generate. A length of 1 generates a 64 byte passwords.
+
+    Returns:
+        A byte array of password.
+    """
+
+    encrypt_key = b""
+    # print("Generating key:")
+    for i in range(length):
+        key = argon2_hash(password_bytes,hmac_salt(password_bytes,bytes(f"salt{i}","UTF-8")))
+        # print(f"\tAppending key[{len(key)}]: ",key.hex())
+        encrypt_key+=key
+    # print(f"Using Encryption Key[{len(encrypt_key)}]: ",encrypt_key.hex())
+    return encrypt_key
+
+def encode(password: bytes, secret: bytes) -> bytes:
+    """
+    Encodes the secret using the password.
+    
+    Params:
+        password: A byte array of the password.
+        secret: A byte array of the text to be encoded.
+
+    Returns:
+        Byte array containing the encoded data.
+    """
+
+    if not isinstance(password,bytes):
+        raise Exception("The password must be bytes.")
+    if not isinstance(secret,bytes):
+        raise Exception("The secret must be bytes.")
+
+   # Create enough keys to encode the text.
+    encrypt_key = make_password(password, len(secret)//64+1)
+    
+    # Mod encode the text
+    encoded = bytes((x+y)%256 for x,y in zip(secret,encrypt_key))
+    
+    return encoded
+
+def decode(password: bytes, secret: bytes) -> str:
+    """
+    Decodes the secret using the provided password.
+    
+    Params:
+        password: A byte array of the password.
+        secret: A byte array representation of the secret.
+
+    Returns:
+        The decoded byte array.
+    """
+    if not isinstance(password, bytes):
+        print("The password must be the type of bytes.")
+    if not isinstance(secret,bytes):
+        print("The secret must be the type of bytes.")
+
+    # Create enough keys to decode the text.
+    encrypt_key = make_password(password, len(secret)//64+1)
+    
+    # Mode decode the text
+    decoded = bytes((x-y)%256 for x,y in zip(secret,encrypt_key))
+
+    return decoded
--- SteganographyCodec.py
+++ SteganographyCodec.py
@@ -9,7 +9,6 @@ def write(fileName: str, targetName: str, data: bytes) -> None:
         targetName: The name of the target image.
         data: A byte array which will be encoded into the image.
     """
-    print(data,"\n",data.hex())
     with Image.open(fileName) as sourceFile:
         pixels = sourceFile.load()
         width,height = sourceFile.size
@@ -32,7 +31,14 @@ def write(fileName: str, targetName: str, data: bytes) -> None:
         sourceFile.save(targetName,"PNG")
 
 def read(fileName: str) -> str:
+    """
+    Reads from the provided file, and returns the decoded byte array.
 
+    Params:
+        fileName: The name of the image to read from.
+    Returns:
+        Byte array containing data.
+    """
     with Image.open(fileName) as sourceFile:
         pixels = sourceFile.load()
         width, height = sourceFile.size
@@ -40,35 +46,15 @@ def read(fileName: str) -> str:
         rgbOffset = 0
         bitOffset = 0
         temp = 0
-        text = b""
+        text = [0]*(width*height*3//8)
         for i in range(width*height*3):
+
             if i % 8 == 0 and i != 0:
-                if temp == 0:
-                    print(text)
-                    break
-                print("\t",format(temp,"08b"))
-                text += temp.to_bytes()
+                text[i//8-1] = temp
                 temp = 0
             pixelData = pixels[i//3%width,i//3//width][rgbOffset%3] & 1
             temp |= pixelData << (i%8)
-            print(format(pixels[i//3%width,i//3//width][rgbOffset%3],"08b"),pixelData, format(pixelData<<(i%8)))
-            # print(f"{i//8}/{width*height*3//8}",pixels[i//3%width,i//3//width][rgbOffset%3],pixelData,pixelData << 7-(i%8),format(temp,"08b"),chr(temp))
 
-            rgbOffset+=1
-            bitOffset
-
-if __name__ == "__main__":
-    choice = input("(W)rite or (R)ead: ")
-    match(choice):
-        case "w" | "W":
-            # filename = input("File to open: ")
-            # text = input("Data to encode into file: ")
-            # text += b"\x00".decode()
-            
-            filename = "fox.png"
-            text = "Hello World"
-
-            write(filename,f"encoded-{filename}",text.encode())
-        case "r" | "R":
-            filename = "encoded-fox.png"
-            read(filename)
+            rgbOffset += 1
+            bitOffset += 1
+    return bytes(text)
