d7817d8a12283a555f8fa30357aec6482bd818f9
DarkFoxGnS <szabotibor123456789@gmail.com>
2025-07-04 16:16:04 +0200

    - PasswordGenerator.py created, requirements drafted.

---
+++ PasswordGenerator.py
@@ -0,0 +1,4 @@
+def generate(characterSet: str, length: int) -> str:
+    import secrets
+    # This must take in a regex of characters, and generate a string based on the input. The text must be generated using secrets and not using random for added security. The string the must be returned to the caller.
+
