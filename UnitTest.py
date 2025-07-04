eb0e9d83ff4af3103e993e52655d7c3e535d58ff
DarkFoxGnS <szabotibor123456789@gmail.com>
2025-07-04 16:12:24 +0200

    UnitTest added for TextEncoder

--- UnitTest.py
+++ UnitTest.py
@@ -1,18 +1,21 @@
 import unittest
 
+iterationCount = 5
+
 class TestStegonography(unittest.TestCase):
     
     def setUp(self):
-        self.iterationCount = 5
+        self.iterationCount = iterationCount
 
     def testRandomIntegerData(self):
         # Used for stress testing the system for faults.
         import random
         import SteganographyCodec as sc
         
-        for j in range(self.iterationCount):
-            with self.subTest(j = j):
-                originalData = bytes([int(random.random()*255+1) for i in range(int(random.random()*255+1))])+b"\x00"
+        for _ in range(self.iterationCount):
+            with self.subTest():
+                # Random data between 1-255
+                originalData = bytes([int(random.random()*254+1) for i in range(int(random.random()*255+1))])+b"\x00"
                 sc.write("unit.png","encoded-unit.png",originalData)
                 readData = sc.read("encoded-unit.png")
                 length = 0
@@ -27,8 +30,8 @@ class TestStegonography(unittest.TestCase):
         import random
         import SteganographyCodec as sc
 
-        for j in range(self.iterationCount):
-            with self.subTest(j = j):
+        for _ in range(self.iterationCount):
+            with self.subTest():
                 originalData = "".join([chr(int(int(b"1111_11",2)*random.random()+40)) for i in range(1000)]).encode()+b"\x00"
                 sc.write("unit.png","encoded-unit.png",originalData)
                 readData = sc.read("encoded-unit.png")
@@ -59,14 +62,48 @@ class TestStegonography(unittest.TestCase):
 
 class TestTextEncoder(unittest.TestCase):
 
-    def setIp(self):
+    def setUp(self):
+        self.iterationCount = iterationCount
         pass
     
     def testRandomIntegerData(self):
-        import TextEncoder as te
+        import DataEncoder as de
+        import random
+        for _ in range(self.iterationCount):
+            with self.subTest():
+                originalData = bytes(int(random.random()*254+1) for x in range(int(random.random()*255+1)))
+                key = bytes(int(random.random()*254+1) for x in range(int(random.random()*255+1)))
+                
+                byteData = de.encode(key,originalData)
+                
+                decodedData = de.decode(key,byteData)
+                self.assertEqual(decodedData,originalData,(originalData,decodedData))
 
-        self.assertTrue(True)
-        pass
+    def testRandomStringData(self):
+        import DataEncoder as de
+        import random
+
+        for _ in range(self.iterationCount):
+            with self.subTest():
+                originalData = "".join([chr(int(int(b"1111_11",2)*random.random()+40)) for i in range(1000)])
+                password = "".join([chr(int(int(b"1111_11",2)*random.random()+40)) for i in range(1000)])
+                
+                byteData = de.encode(password.encode(), originalData.encode())
+                
+                decoded = de.decode(password.encode(), byteData)
+                
+                self.assertEqual(originalData,decoded.decode(),(originalData,decoded.decode()))
+    
+    def testEmptyData(self):
+        import DataEncoder as de
+        
+        originalData = b""
+        password = b""
+
+        encodedData = de.encode(password,originalData)
+        decodedData = de.decode(password,encodedData)
+        
+        self.assertEqual(originalData,decodedData)
 
 if __name__ == "__main__":
     unittest.main()
