68b343eb75c2b6d1960ff37f272dc33960830715
DarkFoxGnS <szabotibor123456789@gmail.com>
2025-07-08 13:17:46 +0200

    Password Generator added.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,4 +1,49 @@
 def generate(characterSet: str, length: int) -> str:
-    import secrets
-    # This must take in a regex of characters, and generate a string based on the input. The text must be generated using secrets and not using random for added security. The string the must be returned to the caller.
+    """
+    Generates a random text using the given character set.
 
+    Params:
+        characterSet: The set from which the characters will be selected for the password.
+        length: The length of password to be generated.
+
+    Returns:
+        A string of the generated password.
+
+    Example:
+        generate("abcdefghijklmnopqrstuvwxyz",20)
+    """
+    import secrets
+    password = ""
+
+    for i in range(length):
+        password += secrets.choice(characterSet)
+
+    return password
+
+def generateCustom(textInput: str, length: int) -> str:
+    """
+    Generates a random text using a simplified character markdown.
+    
+    Params:
+        textInput: Custom characterset input using the markdown.
+        length: The length of the password to be generated.
+
+    Returns:
+        A string of the generated password.
+
+    Example:
+        generateCustom("a-zA-Z0-9\\\-?!%/*+-",30)
+    """
+    import re
+    characterSet = ""
+    regexText = r"\\([\\-])|(.)-(.)|(.)"
+    for match in re.findall(regexText,textInput):
+        if match[0]:
+            characterSet += match[0]
+        elif match[3]:
+            characterSet += match[3]
+        else:
+            for i in range(ord(match[1]),ord(match[2])):
+                characterSet += chr(i)
+    
+    return generate(characterSet, length)
